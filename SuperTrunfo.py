ef600f1433bbfabf23473a89b38d62e5bb791a5c
Towid1 <mlopezbarros68@gmail.com>
2026-05-05 01:55:10 -0300

    fix: Fim do jogo realmente adicionado

--- SuperTrunfo.py
+++ SuperTrunfo.py
@@ -6,7 +6,7 @@ def turno(vez,carta):
         print("Sua carta atual:",carta)
         print("Qual atributo você quer comparar?")
         while True:
-            atributo = int(input("Número da dex (1), IV total (2) ou Peso (3): "))
+            atributo = int(input("(1)Número da dex, (2)IV total ou (3)Peso: "))
             if atributo == 1 or atributo == 2 or atributo == 3:
                 return atributo
             else:
@@ -20,6 +20,12 @@ def nome_atributo(n):
     elif n == 3:
         return "Peso"
 
+def teste_de_vitória(p1,p2,baralho):
+     if len(p1) == len(baralho):
+          return "Jogador 1"
+     elif len(p2) == len(baralho):
+          return "Jogador 2"
+
 while True:
     n_jogadores = str(input("Número de jogadores(1 ou 2): "))
     if n_jogadores == "1" or n_jogadores == "2":
@@ -29,36 +35,36 @@ while True:
 
 #   ["Nome",DEX,IVtotal,Peso]
 baralho = [
-    #["Mudkip",258,310,7.6],
-    #["Basculegion",902,530,110],
-    #["Shinx",403,263,9.5],
-    #["Regice",378,580,175],
-    #["Algomon",1022,590,162.5],
-    #["Purrloin",509,281,10.1],
-    #["Drifloon",425,348,1.2],
-    #["Pangoro",675,495,136],
-    #["Amoonguss",591,464,10.5],
-    #["Mudbray",749,385,110],
-    #["Clodsire",980,430,223],
-    #["Magcargo",219,430,55],
-    #["Riolu",447,285,20.2],
-    #["Kyurem",646,660,325],
-    #["Kyogre",382,670,352],
-    #["Zeraora",807,600,44.5],
-    #["Yvetal",717,680,203],
-    #["Mewtwo",150,680,122],
-    #["Lugia",249,680,216],
-    #["Arceus",493,720,320],
-    #["Rayquaza",384,680,206.5],
-    #["Venipede",543,260,5.3],
-    #["Dragonite",149,600,210],
-    #["Machop",66, 305, 19.5],
-    #["Wailmer",320,400,130],
-    #["Groundon",383,670,352],
-    #["Darkrai",491,600,50.5],
-    #["Scraggy",559,348,11.8],
-    #["Annihilape",979,535,56],
-    #["Falinks",870,470,62.0],
+    ["Mudkip",258,310,7.6],
+    ["Basculegion",902,530,110],
+    ["Shinx",403,263,9.5],
+    ["Regice",378,580,175],
+    ["Algomon",1022,590,162.5],
+    ["Purrloin",509,281,10.1],
+    ["Drifloon",425,348,1.2],
+    ["Pangoro",675,495,136],
+    ["Amoonguss",591,464,10.5],
+    ["Mudbray",749,385,110],
+    ["Clodsire",980,430,223],
+    ["Magcargo",219,430,55],
+    ["Riolu",447,285,20.2],
+    ["Kyurem",646,660,325],
+    ["Kyogre",382,670,352],
+    ["Zeraora",807,600,44.5],
+    ["Yvetal",717,680,203],
+    ["Mewtwo",150,680,122],
+    ["Lugia",249,680,216],
+    ["Arceus",493,720,320],
+    ["Rayquaza",384,680,206.5],
+    ["Venipede",543,260,5.3],
+    ["Dragonite",149,600,210],
+    ["Machop",66, 305, 19.5],
+    ["Falinks",870,470,62.0],
+    ["Wailmer",320,400,130],
+    ["Groundon",383,670,352],
+    ["Darkrai",491,600,50.5],
+    ["Scraggy",559,348,11.8],
+    ["Annihilape",979,535,56],
     ["Zoroark",571,510,81.1],
     ["Druddigon",621,485,139]
 ]
@@ -68,9 +74,11 @@ jogador_1 = baralho[:len(baralho)//2]
 jogador_2 = baralho[len(baralho)//2:len(baralho)]
 monte_espera = []
 vez = 1
+rodadas = 0
 
 
 while len(jogador_1) != 0 and len(jogador_2) != 0:
+    rodadas += 1
     carta1 = jogador_1.pop(0)
     carta2 = jogador_2.pop(0)
 
@@ -101,18 +109,20 @@ while len(jogador_1) != 0 and len(jogador_2) != 0:
             atributo = turno(2,carta2)
             vez = 1
 
-# TESTE DE VITÓRIA
+# DISPUTA DA RODADA
     if carta1[atributo] > carta2[atributo]:
         jogador_1 += monte_espera + [carta1] + [carta2]
         monte_espera = []
         print(f"O {nome_atributo(atributo)} de {carta1[0]} ({carta1[atributo]}) é maior do que {carta2[0]} ({carta2[atributo]})")
-        print(f"Jogador 1 ganhou a rodada! E esta com {len(jogador_1)} cartas.")
+        print(f"Jogador 1 ganhou a rodada! E esta com {len(jogador_1)} cartas de {len(baralho)}.")
     elif carta2[atributo] > carta1[atributo]:
         jogador_2 += monte_espera + [carta2] + [carta1]
         monte_espera = []
         print(f"O {nome_atributo(atributo)} de {carta2[0]} ({carta2[atributo]}) é maior do que {carta1[0]} ({carta1[atributo]})")
-        print(f"Jogador 2 ganhou a rodada! E esta com {len(jogador_2)} cartas.")
+        print(f"Jogador 2 ganhou a rodada! E esta com {len(jogador_2)} cartas de {len(baralho)}.")
     else:
         monte_espera += [carta1] + [carta2]
         print(f"O {nome_atributo(atributo)} de {carta1[0]} e {carta2[0]} são iguais ({carta1[atributo]})")
         print(f"Empate! Monte de espera: {monte_espera}")
+
+print(f"{teste_de_vitória(jogador_1,jogador_2,baralho)} venceu! Em {rodadas} rodadas!")
--- teste.py
+++ teste.py
@@ -1,9 +1,19 @@
-lista = [[1,2,3],[4,5,6]]
-lista2 =[[7,8,9],[10,11,12]]
+# JOGADOR 1
+def turno(vez,carta):
+        input(f"Vez do jogador {vez} Pressione enter para continuar!")
+        print("Sua carta atual:",carta)
+        print("Qual atributo você quer comparar?")
+        while True:
+            atributo = int(input("Número da dex(1), IV total(2) ou Peso(3): "))
+            if atributo == 1 or atributo == 2 or atributo == 3:
+                return atributo
+            else:
+                print("Valor inválido! Tente novamente.")
 
-tirar = list(lista.pop(0))
-
-print(lista,"diferença",tirar)
-
-print(lista + lista2)
-print([tirar , lista])
+def teste_de_vitória(p1,p2,baralho):
+     if len(p1) == len(baralho):
+          return "Jogador 1"
+     elif len(p2) == len(baralho):
+          return "Jogador 2"
+ 
+    
